7dbda2b2b167148e7c950d5b503c9aa33c0c6d59
sim511777 <sim51177@gmail.com>
2019-03-18 09:57:10 +0900

    indent formatting 수정

--- FontViewer/FormMain.Designer.cs
+++ FontViewer/FormMain.Designer.cs
@@ -1,216 +1,216 @@
 ﻿namespace FontViewer {
-   partial class FormMain {
-      /// <summary>
-      /// 필수 디자이너 변수입니다.
-      /// </summary>
-      private System.ComponentModel.IContainer components = null;
+    partial class FormMain {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
 
-      /// <summary>
-      /// 사용 중인 모든 리소스를 정리합니다.
-      /// </summary>
-      /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
-      protected override void Dispose(bool disposing) {
-         if (disposing && (components != null)) {
-            components.Dispose();
-         }
-         base.Dispose(disposing);
-      }
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
 
-      #region Windows Form 디자이너에서 생성한 코드
+        #region Windows Form 디자이너에서 생성한 코드
 
-      /// <summary>
-      /// 디자이너 지원에 필요한 메서드입니다. 
-      /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
-      /// </summary>
-      private void InitializeComponent() {
-         this.panel1 = new System.Windows.Forms.Panel();
-         this.tbxExample = new System.Windows.Forms.TextBox();
-         this.numFontSize = new System.Windows.Forms.NumericUpDown();
-         this.lvwFont = new System.Windows.Forms.ListView();
-         this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-         this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-         this.panel2 = new System.Windows.Forms.Panel();
-         this.tbxDir = new System.Windows.Forms.TextBox();
-         this.btnDir = new System.Windows.Forms.Button();
-         this.btnHelp = new System.Windows.Forms.Button();
-         this.rdoFolder = new System.Windows.Forms.RadioButton();
-         this.rdoSystem = new System.Windows.Forms.RadioButton();
-         this.dlgFolder = new System.Windows.Forms.FolderBrowserDialog();
-         this.panel1.SuspendLayout();
-         ((System.ComponentModel.ISupportInitialize)(this.numFontSize)).BeginInit();
-         this.panel2.SuspendLayout();
-         this.SuspendLayout();
-         // 
-         // panel1
-         // 
-         this.panel1.Controls.Add(this.tbxExample);
-         this.panel1.Controls.Add(this.numFontSize);
-         this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
-         this.panel1.Location = new System.Drawing.Point(0, 21);
-         this.panel1.Name = "panel1";
-         this.panel1.Size = new System.Drawing.Size(790, 21);
-         this.panel1.TabIndex = 0;
-         // 
-         // tbxExample
-         // 
-         this.tbxExample.Dock = System.Windows.Forms.DockStyle.Fill;
-         this.tbxExample.Location = new System.Drawing.Point(53, 0);
-         this.tbxExample.Name = "tbxExample";
-         this.tbxExample.Size = new System.Drawing.Size(737, 21);
-         this.tbxExample.TabIndex = 1;
-         this.tbxExample.Text = "brown fox jumps over the lazy dog";
-         this.tbxExample.TextChanged += new System.EventHandler(this.tbxExample_TextChanged);
-         // 
-         // numFontSize
-         // 
-         this.numFontSize.Dock = System.Windows.Forms.DockStyle.Left;
-         this.numFontSize.Location = new System.Drawing.Point(0, 0);
-         this.numFontSize.Minimum = new decimal(new int[] {
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent() {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.tbxExample = new System.Windows.Forms.TextBox();
+            this.numFontSize = new System.Windows.Forms.NumericUpDown();
+            this.lvwFont = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.tbxDir = new System.Windows.Forms.TextBox();
+            this.btnDir = new System.Windows.Forms.Button();
+            this.btnHelp = new System.Windows.Forms.Button();
+            this.rdoFolder = new System.Windows.Forms.RadioButton();
+            this.rdoSystem = new System.Windows.Forms.RadioButton();
+            this.dlgFolder = new System.Windows.Forms.FolderBrowserDialog();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numFontSize)).BeginInit();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.tbxExample);
+            this.panel1.Controls.Add(this.numFontSize);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 21);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(790, 21);
+            this.panel1.TabIndex = 0;
+            // 
+            // tbxExample
+            // 
+            this.tbxExample.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tbxExample.Location = new System.Drawing.Point(53, 0);
+            this.tbxExample.Name = "tbxExample";
+            this.tbxExample.Size = new System.Drawing.Size(737, 21);
+            this.tbxExample.TabIndex = 1;
+            this.tbxExample.Text = "brown fox jumps over the lazy dog";
+            this.tbxExample.TextChanged += new System.EventHandler(this.tbxExample_TextChanged);
+            // 
+            // numFontSize
+            // 
+            this.numFontSize.Dock = System.Windows.Forms.DockStyle.Left;
+            this.numFontSize.Location = new System.Drawing.Point(0, 0);
+            this.numFontSize.Minimum = new decimal(new int[] {
             1,
             0,
             0,
             0});
-         this.numFontSize.Name = "numFontSize";
-         this.numFontSize.Size = new System.Drawing.Size(53, 21);
-         this.numFontSize.TabIndex = 2;
-         this.numFontSize.Value = new decimal(new int[] {
+            this.numFontSize.Name = "numFontSize";
+            this.numFontSize.Size = new System.Drawing.Size(53, 21);
+            this.numFontSize.TabIndex = 2;
+            this.numFontSize.Value = new decimal(new int[] {
             10,
             0,
             0,
             0});
-         this.numFontSize.ValueChanged += new System.EventHandler(this.numFontSize_ValueChanged);
-         // 
-         // lvwFont
-         // 
-         this.lvwFont.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.numFontSize.ValueChanged += new System.EventHandler(this.numFontSize_ValueChanged);
+            // 
+            // lvwFont
+            // 
+            this.lvwFont.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
             this.columnHeader1,
             this.columnHeader2});
-         this.lvwFont.Dock = System.Windows.Forms.DockStyle.Fill;
-         this.lvwFont.GridLines = true;
-         this.lvwFont.Location = new System.Drawing.Point(0, 42);
-         this.lvwFont.Name = "lvwFont";
-         this.lvwFont.Size = new System.Drawing.Size(790, 604);
-         this.lvwFont.TabIndex = 1;
-         this.lvwFont.UseCompatibleStateImageBehavior = false;
-         this.lvwFont.View = System.Windows.Forms.View.Details;
-         // 
-         // columnHeader1
-         // 
-         this.columnHeader1.Text = "Font Name";
-         this.columnHeader1.Width = 151;
-         // 
-         // columnHeader2
-         // 
-         this.columnHeader2.Text = "Example";
-         this.columnHeader2.Width = 1000;
-         // 
-         // panel2
-         // 
-         this.panel2.Controls.Add(this.tbxDir);
-         this.panel2.Controls.Add(this.btnDir);
-         this.panel2.Controls.Add(this.btnHelp);
-         this.panel2.Controls.Add(this.rdoFolder);
-         this.panel2.Controls.Add(this.rdoSystem);
-         this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
-         this.panel2.Location = new System.Drawing.Point(0, 0);
-         this.panel2.Name = "panel2";
-         this.panel2.Size = new System.Drawing.Size(790, 21);
-         this.panel2.TabIndex = 2;
-         // 
-         // tbxDir
-         // 
-         this.tbxDir.BackColor = System.Drawing.SystemColors.Window;
-         this.tbxDir.Dock = System.Windows.Forms.DockStyle.Fill;
-         this.tbxDir.Location = new System.Drawing.Point(124, 0);
-         this.tbxDir.Name = "tbxDir";
-         this.tbxDir.ReadOnly = true;
-         this.tbxDir.Size = new System.Drawing.Size(616, 21);
-         this.tbxDir.TabIndex = 3;
-         this.tbxDir.Visible = false;
-         this.tbxDir.TextChanged += new System.EventHandler(this.tbxDir_TextChanged);
-         // 
-         // btnDir
-         // 
-         this.btnDir.Dock = System.Windows.Forms.DockStyle.Right;
-         this.btnDir.Location = new System.Drawing.Point(740, 0);
-         this.btnDir.Name = "btnDir";
-         this.btnDir.Size = new System.Drawing.Size(27, 21);
-         this.btnDir.TabIndex = 2;
-         this.btnDir.Text = "...";
-         this.btnDir.UseVisualStyleBackColor = true;
-         this.btnDir.Visible = false;
-         this.btnDir.Click += new System.EventHandler(this.btnDir_Click);
-         // 
-         // btnHelp
-         // 
-         this.btnHelp.Dock = System.Windows.Forms.DockStyle.Right;
-         this.btnHelp.Location = new System.Drawing.Point(767, 0);
-         this.btnHelp.Name = "btnHelp";
-         this.btnHelp.Size = new System.Drawing.Size(23, 21);
-         this.btnHelp.TabIndex = 4;
-         this.btnHelp.Text = "?";
-         this.btnHelp.UseVisualStyleBackColor = true;
-         this.btnHelp.Click += new System.EventHandler(this.btnHelp_Click);
-         // 
-         // rdoFolder
-         // 
-         this.rdoFolder.AutoSize = true;
-         this.rdoFolder.Dock = System.Windows.Forms.DockStyle.Left;
-         this.rdoFolder.Location = new System.Drawing.Point(66, 0);
-         this.rdoFolder.Name = "rdoFolder";
-         this.rdoFolder.Size = new System.Drawing.Size(58, 21);
-         this.rdoFolder.TabIndex = 1;
-         this.rdoFolder.Text = "Folder";
-         this.rdoFolder.UseVisualStyleBackColor = true;
-         // 
-         // rdoSystem
-         // 
-         this.rdoSystem.AutoSize = true;
-         this.rdoSystem.Checked = true;
-         this.rdoSystem.Dock = System.Windows.Forms.DockStyle.Left;
-         this.rdoSystem.Location = new System.Drawing.Point(0, 0);
-         this.rdoSystem.Name = "rdoSystem";
-         this.rdoSystem.Size = new System.Drawing.Size(66, 21);
-         this.rdoSystem.TabIndex = 0;
-         this.rdoSystem.TabStop = true;
-         this.rdoSystem.Text = "System";
-         this.rdoSystem.UseVisualStyleBackColor = true;
-         this.rdoSystem.CheckedChanged += new System.EventHandler(this.rdoSystem_CheckedChanged);
-         // 
-         // FormMain
-         // 
-         this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
-         this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-         this.ClientSize = new System.Drawing.Size(790, 646);
-         this.Controls.Add(this.lvwFont);
-         this.Controls.Add(this.panel1);
-         this.Controls.Add(this.panel2);
-         this.Name = "FormMain";
-         this.Text = "Font Viewer";
-         this.panel1.ResumeLayout(false);
-         this.panel1.PerformLayout();
-         ((System.ComponentModel.ISupportInitialize)(this.numFontSize)).EndInit();
-         this.panel2.ResumeLayout(false);
-         this.panel2.PerformLayout();
-         this.ResumeLayout(false);
+            this.lvwFont.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lvwFont.GridLines = true;
+            this.lvwFont.Location = new System.Drawing.Point(0, 42);
+            this.lvwFont.Name = "lvwFont";
+            this.lvwFont.Size = new System.Drawing.Size(790, 604);
+            this.lvwFont.TabIndex = 1;
+            this.lvwFont.UseCompatibleStateImageBehavior = false;
+            this.lvwFont.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Font Name";
+            this.columnHeader1.Width = 151;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Example";
+            this.columnHeader2.Width = 1000;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.tbxDir);
+            this.panel2.Controls.Add(this.btnDir);
+            this.panel2.Controls.Add(this.btnHelp);
+            this.panel2.Controls.Add(this.rdoFolder);
+            this.panel2.Controls.Add(this.rdoSystem);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(790, 21);
+            this.panel2.TabIndex = 2;
+            // 
+            // tbxDir
+            // 
+            this.tbxDir.BackColor = System.Drawing.SystemColors.Window;
+            this.tbxDir.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tbxDir.Location = new System.Drawing.Point(124, 0);
+            this.tbxDir.Name = "tbxDir";
+            this.tbxDir.ReadOnly = true;
+            this.tbxDir.Size = new System.Drawing.Size(616, 21);
+            this.tbxDir.TabIndex = 3;
+            this.tbxDir.Visible = false;
+            this.tbxDir.TextChanged += new System.EventHandler(this.tbxDir_TextChanged);
+            // 
+            // btnDir
+            // 
+            this.btnDir.Dock = System.Windows.Forms.DockStyle.Right;
+            this.btnDir.Location = new System.Drawing.Point(740, 0);
+            this.btnDir.Name = "btnDir";
+            this.btnDir.Size = new System.Drawing.Size(27, 21);
+            this.btnDir.TabIndex = 2;
+            this.btnDir.Text = "...";
+            this.btnDir.UseVisualStyleBackColor = true;
+            this.btnDir.Visible = false;
+            this.btnDir.Click += new System.EventHandler(this.btnDir_Click);
+            // 
+            // btnHelp
+            // 
+            this.btnHelp.Dock = System.Windows.Forms.DockStyle.Right;
+            this.btnHelp.Location = new System.Drawing.Point(767, 0);
+            this.btnHelp.Name = "btnHelp";
+            this.btnHelp.Size = new System.Drawing.Size(23, 21);
+            this.btnHelp.TabIndex = 4;
+            this.btnHelp.Text = "?";
+            this.btnHelp.UseVisualStyleBackColor = true;
+            this.btnHelp.Click += new System.EventHandler(this.btnHelp_Click);
+            // 
+            // rdoFolder
+            // 
+            this.rdoFolder.AutoSize = true;
+            this.rdoFolder.Dock = System.Windows.Forms.DockStyle.Left;
+            this.rdoFolder.Location = new System.Drawing.Point(66, 0);
+            this.rdoFolder.Name = "rdoFolder";
+            this.rdoFolder.Size = new System.Drawing.Size(58, 21);
+            this.rdoFolder.TabIndex = 1;
+            this.rdoFolder.Text = "Folder";
+            this.rdoFolder.UseVisualStyleBackColor = true;
+            // 
+            // rdoSystem
+            // 
+            this.rdoSystem.AutoSize = true;
+            this.rdoSystem.Checked = true;
+            this.rdoSystem.Dock = System.Windows.Forms.DockStyle.Left;
+            this.rdoSystem.Location = new System.Drawing.Point(0, 0);
+            this.rdoSystem.Name = "rdoSystem";
+            this.rdoSystem.Size = new System.Drawing.Size(66, 21);
+            this.rdoSystem.TabIndex = 0;
+            this.rdoSystem.TabStop = true;
+            this.rdoSystem.Text = "System";
+            this.rdoSystem.UseVisualStyleBackColor = true;
+            this.rdoSystem.CheckedChanged += new System.EventHandler(this.rdoSystem_CheckedChanged);
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(790, 646);
+            this.Controls.Add(this.lvwFont);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel2);
+            this.Name = "FormMain";
+            this.Text = "Font Viewer";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numFontSize)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
 
-      }
+        }
 
-      #endregion
+        #endregion
 
-      private System.Windows.Forms.Panel panel1;
-      private System.Windows.Forms.TextBox tbxExample;
-      private System.Windows.Forms.NumericUpDown numFontSize;
-      private System.Windows.Forms.ListView lvwFont;
-      private System.Windows.Forms.ColumnHeader columnHeader1;
-      private System.Windows.Forms.ColumnHeader columnHeader2;
-      private System.Windows.Forms.Panel panel2;
-      private System.Windows.Forms.TextBox tbxDir;
-      private System.Windows.Forms.Button btnDir;
-      private System.Windows.Forms.RadioButton rdoFolder;
-      private System.Windows.Forms.RadioButton rdoSystem;
-      private System.Windows.Forms.FolderBrowserDialog dlgFolder;
-      private System.Windows.Forms.Button btnHelp;
-   }
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox tbxExample;
+        private System.Windows.Forms.NumericUpDown numFontSize;
+        private System.Windows.Forms.ListView lvwFont;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.TextBox tbxDir;
+        private System.Windows.Forms.Button btnDir;
+        private System.Windows.Forms.RadioButton rdoFolder;
+        private System.Windows.Forms.RadioButton rdoSystem;
+        private System.Windows.Forms.FolderBrowserDialog dlgFolder;
+        private System.Windows.Forms.Button btnHelp;
+    }
 }
 
--- FontViewer/FormMain.cs
+++ FontViewer/FormMain.cs
@@ -11,88 +11,88 @@ using System.Drawing.Text;
 using System.IO;
 
 namespace FontViewer {
-   public partial class FormMain : Form {
-      public FormMain() {
-         InitializeComponent();
-         this.ReDrawFonts();
-      }
+    public partial class FormMain : Form {
+        public FormMain() {
+            InitializeComponent();
+            this.ReDrawFonts();
+        }
 
-      
-      private void ReDrawFonts() {
-         FontFamily[] fonts = null;
-         if (this.rdoSystem.Checked) {
-            fonts = new InstalledFontCollection().Families; 
-         } else {
-            string dir = this.tbxDir.Text;
-            if (Directory.Exists(dir) == false)
-               fonts = new FontFamily[0];
-            else {
-               var files = Directory.GetFiles(dir);
-               PrivateFontCollection clt = new PrivateFontCollection();
-               foreach (var file in files) {
-                  clt.AddFontFile(file);
-               }
-               fonts = clt.Families;
+
+        private void ReDrawFonts() {
+            FontFamily[] fonts = null;
+            if (this.rdoSystem.Checked) {
+                fonts = new InstalledFontCollection().Families;
+            } else {
+                string dir = this.tbxDir.Text;
+                if (Directory.Exists(dir) == false)
+                    fonts = new FontFamily[0];
+                else {
+                    var files = Directory.GetFiles(dir);
+                    PrivateFontCollection clt = new PrivateFontCollection();
+                    foreach (var file in files) {
+                        clt.AddFontFile(file);
+                    }
+                    fonts = clt.Families;
+                }
             }
-         }
 
-         string example = this.tbxExample.Text;
-         float fontSize = (float)this.numFontSize.Value;
-         this.lvwFont.Font = new Font(SystemFonts.DialogFont.FontFamily, fontSize, FontStyle.Regular);
-         
-         this.lvwFont.Items.Clear();
-         this.lvwFont.BeginUpdate();
-         foreach(var font in fonts) {
-            if (font.Name == string.Empty)
-               continue;
-            ListViewItem item = new ListViewItem(new string[]{font.Name, example});
-            item.UseItemStyleForSubItems = false;
-            item.SubItems[0].Font = new Font(SystemFonts.DialogFont.FontFamily, fontSize);
-            item.SubItems[1].Font = new Font(font, fontSize);
-            this.lvwFont.Items.Add(item);
-         }
-         
-         this.lvwFont.EndUpdate();
-      }
+            string example = this.tbxExample.Text;
+            float fontSize = (float)this.numFontSize.Value;
+            this.lvwFont.Font = new Font(SystemFonts.DialogFont.FontFamily, fontSize, FontStyle.Regular);
 
-      private void rdoSystem_CheckedChanged(object sender, EventArgs e) {
-         if (this.rdoSystem.Checked) {
-            this.tbxDir.Visible = false;
-            this.btnDir.Visible = false;
-         } else {
-            this.tbxDir.Visible = true;
-            this.btnDir.Visible = true;
-         }
-         this.ReDrawFonts();
-      }
+            this.lvwFont.Items.Clear();
+            this.lvwFont.BeginUpdate();
+            foreach (var font in fonts) {
+                if (font.Name == string.Empty)
+                    continue;
+                ListViewItem item = new ListViewItem(new string[] { font.Name, example });
+                item.UseItemStyleForSubItems = false;
+                item.SubItems[0].Font = new Font(SystemFonts.DialogFont.FontFamily, fontSize);
+                item.SubItems[1].Font = new Font(font, fontSize);
+                this.lvwFont.Items.Add(item);
+            }
 
-      private void numFontSize_ValueChanged(object sender, EventArgs e) {
-         this.ReDrawFonts();
-      }
+            this.lvwFont.EndUpdate();
+        }
 
-      private void tbxExample_TextChanged(object sender, EventArgs e) {
-         this.ReDrawFonts();
-      }
+        private void rdoSystem_CheckedChanged(object sender, EventArgs e) {
+            if (this.rdoSystem.Checked) {
+                this.tbxDir.Visible = false;
+                this.btnDir.Visible = false;
+            } else {
+                this.tbxDir.Visible = true;
+                this.btnDir.Visible = true;
+            }
+            this.ReDrawFonts();
+        }
 
-      private void tbxDir_TextChanged(object sender, EventArgs e) {
-         this.ReDrawFonts();
-      }
+        private void numFontSize_ValueChanged(object sender, EventArgs e) {
+            this.ReDrawFonts();
+        }
 
-      private void btnDir_Click(object sender, EventArgs e) {
-         if (dlgFolder.ShowDialog(this) != DialogResult.OK)
-            return;
-         this.tbxDir.Text = this.dlgFolder.SelectedPath;
-      }
+        private void tbxExample_TextChanged(object sender, EventArgs e) {
+            this.ReDrawFonts();
+        }
 
-      private void btnHelp_Click(object sender, EventArgs e) {
-         var caption = "Font viewer";
-         var link = "https://github.com/sim511777/FontViewer";
-         var message = link + "\r\nWould you visit?";
+        private void tbxDir_TextChanged(object sender, EventArgs e) {
+            this.ReDrawFonts();
+        }
 
-         var dr = MessageBox.Show(message, caption, MessageBoxButtons.YesNo);
-         if (dr == DialogResult.Yes) {
-            System.Diagnostics.Process.Start(link);
-         }
-      }
-   }
+        private void btnDir_Click(object sender, EventArgs e) {
+            if (dlgFolder.ShowDialog(this) != DialogResult.OK)
+                return;
+            this.tbxDir.Text = this.dlgFolder.SelectedPath;
+        }
+
+        private void btnHelp_Click(object sender, EventArgs e) {
+            var caption = "Font viewer";
+            var link = "https://github.com/sim511777/FontViewer";
+            var message = link + "\r\nWould you visit?";
+
+            var dr = MessageBox.Show(message, caption, MessageBoxButtons.YesNo);
+            if (dr == DialogResult.Yes) {
+                System.Diagnostics.Process.Start(link);
+            }
+        }
+    }
 }
